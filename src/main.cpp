e2e1fa577e3f545e0f1b42827b8a868b770e542b
Vit Knobloch <knoblvit@fel.cvut.cz>
2025-03-19 15:59:27 +0100

    Commented out VRP-TW compilation because it is unfinished

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #include "CVRP/setup.h"
 #include "TSP/setup.h"
-#include "VRP-TW/setup.h"
+// #include "VRP-TW/setup.h"
 #include "common/portfolio.h"
 #include "json.hpp"
 #include "common/logger.h"
@@ -13,17 +13,21 @@
 
 using JSON = nlohmann::json;
 
-JSON loadConfig(const char *config_filename){
+JSON loadConfig(const char *config_filename)
+{
   std::ifstream file(config_filename);
-  if(!file){
+  if (!file)
+  {
     std::cerr << "Couldn't open config file " << config_filename << std::endl;
     exit(100);
   }
   JSON config_json;
-  try{
+  try
+  {
     file >> config_json;
   }
-  catch(const nlohmann::json::exception &ex){
+  catch (const nlohmann::json::exception &ex)
+  {
     std::cerr << "Couldn't read JSON object in config file " << config_filename << std::endl;
     exit(100);
   }
@@ -31,18 +35,22 @@ JSON loadConfig(const char *config_filename){
   return config_json;
 }
 
-int main(int argc, char* argv[]){
-  if(argc < 4){
+int main(int argc, char *argv[])
+{
+  if (argc < 4)
+  {
     std::cerr << "Usage: ./Heuristic configFilename instanceFilename logfile [randomSeed]" << std::endl;
   }
-  const char* config_filename = argv[1];
-  const char* instance_filename = argv[2];
-  const char* log_filename = argv[3];
+  const char *config_filename = argv[1];
+  const char *instance_filename = argv[2];
+  const char *log_filename = argv[3];
 
-  if(argc > 4){
+  if (argc > 4)
+  {
     errno = 0;
     const int seed = (int)strtol(argv[4], nullptr, 10);
-    if(errno != 0){
+    if (errno != 0)
+    {
       std::cerr << "Seed argument present but has invalid value: " << argv[4] << std::endl;
       exit(100);
     }
@@ -52,28 +60,36 @@ int main(int argc, char* argv[]){
 
   JSON config_json = loadConfig(config_filename);
   std::shared_ptr<HeuristicPortfolio> portfolio = nullptr;
-  if(!config_json.contains("problem")){
+  if (!config_json.contains("problem"))
+  {
     std::cerr << "Problem type not specified in config" << std::endl;
     exit(100);
   }
-  else if(config_json["problem"] == "TSP"){
+  else if (config_json["problem"] == "TSP")
+  {
     auto setup = SetupTSP();
     portfolio = setup.preparePortfolio(config_json["heuristics"], instance_filename);
   }
-  else if(config_json["problem"] == "CVRP"){
+  else if (config_json["problem"] == "CVRP")
+  {
     auto setup = SetupCVRP();
     portfolio = setup.preparePortfolio(config_json["heuristics"], instance_filename);
   }
-  else if(config_json["problem"] == "VRP-TW"){
-    auto setup = SetupVRPTW();
-    portfolio = setup.preparePortfolio(config_json["heuristics"], instance_filename);
+  else if (config_json["problem"] == "VRP-TW")
+  {
+    // auto setup = SetupVRPTW();
+    // portfolio = setup.preparePortfolio(config_json["heuristics"], instance_filename);
+    std::cerr << "VRP-TW not implemented yet" << std::endl;
+    exit(100);
   }
-  else{
+  else
+  {
     std::cerr << "Unsupported problem type: " << config_json["problem"] << std::endl;
     exit(100);
   }
 
-  if(portfolio == nullptr){
+  if (portfolio == nullptr)
+  {
     std::cerr << "Problem opening instance " << instance_filename << std::endl;
   }
 
